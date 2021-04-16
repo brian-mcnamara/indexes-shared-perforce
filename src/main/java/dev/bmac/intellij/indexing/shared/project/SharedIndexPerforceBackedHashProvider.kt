969f470531f82602c6d7c90512e62663155b3db1
Brian McNamara <brian.mcnamara@salesforce.com>
2021-04-15 23:25:55 -0700

    Fix hashing strategy

--- src/main/java/dev/bmac/intellij/indexing/shared/project/SharedIndexPerforceBackedHashProvider.kt
+++ src/main/java/dev/bmac/intellij/indexing/shared/project/SharedIndexPerforceBackedHashProvider.kt
@@ -1,5 +1,6 @@
 package dev.bmac.intellij.indexing.shared.project
 
+import com.google.common.hash.Hashing
 import com.intellij.indexing.shared.platform.hash.SharedIndexContentHash
 import com.intellij.indexing.shared.platform.hash.SharedIndexContentHashProvider
 import com.intellij.openapi.application.ApplicationManager
@@ -7,12 +8,16 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.util.indexing.IndexedFile
 import org.jetbrains.idea.perforce.perforce.CommandArguments
+import org.jetbrains.idea.perforce.perforce.FStat
 import org.jetbrains.idea.perforce.perforce.P4File
 import org.jetbrains.idea.perforce.perforce.PerforceRunner
 import org.jetbrains.idea.perforce.perforce.connections.PerforceConnectionManager
-import java.io.*
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.LineNumberReader
 import java.nio.ByteBuffer
-import java.nio.file.Path
+import java.nio.charset.Charset
 import java.util.concurrent.Future
 import java.util.concurrent.atomic.AtomicReference
 
@@ -27,13 +32,18 @@ class SharedIndexPerforceBackedHashProvider : SharedIndexContentHashProvider {
                 future = ApplicationManager.getApplication().executeOnPooledThread{ buildFileMap(content.project) }
             } else {
                 if (future?.isDone == true) {
-                    //TODO
+                    //TODO handle failures
                 }
             }
         } else {
-            val path = content.file.fileSystem.getNioPath(content.file)
-            val revision = fileMap[path] ?: return null
-            return ByteBuffer.allocate(Long.SIZE_BYTES).putLong(revision).array()
+            val path = content.file.fileSystem.getNioPath(content.file)?.toString() ?: return null
+            val fstat = fileMap[path] ?: return null
+
+            //TODO figure out hashing
+            return ByteBuffer.allocate(info.hashLength)
+                    .putLong(fstat.haveRev.toULong().toLong())
+                    .put(Hashing.sha1().hashString(fstat.depotFile, Charset.defaultCharset()).asBytes())
+                    .array()
         }
         return null
     }
@@ -45,46 +55,59 @@ class SharedIndexPerforceBackedHashProvider : SharedIndexContentHashProvider {
 
     companion object {
         private val LOGGER = Logger.getInstance(SharedIndexPerforceBackedHashProvider::class.java)
+        private val FSTAT_DEPOT_PREFIX = "... depotFile "
         private val FSTAT_CLIENT_PREFIX = "... clientFile "
         private val FSTAT_HAVEREV_PREFIX = "... haveRev "
-        private val fileMapRef = AtomicReference<Map<Path, Long>?>()
+        private val fileMapRef = AtomicReference<Map<String, FStat>?>()
         private var future : Future<*>? = null
 
-        private fun getHaveList(project: Project) : Map<Path, Long> {
+        private fun getHaveList(project: Project) : Map<String, FStat> {
+            //TODO is this "true" project root?
             val perforceRoot = P4File.create(project.projectFile!!.parent.parent)
             val runner = PerforceRunner.getInstance(project)
             val commandArguments = CommandArguments()
             //cstat returns a list of changes the client is aware of, which if it has (#have) them, can be used to determine
             //the latest the client has. Also there exists fstatBulk in the plugin, but we only need clientFile and haveRev
             //so using this to reduce the memory overhead for large codebases. Should add this back to perforce plugin
-            commandArguments.append("fstat").append("-T clientFile,haveRev").append(perforceRoot.recursivePath + "#have")
+            commandArguments.append("fstat").append("-T depotFile,clientFile,haveRev").append(perforceRoot.recursivePath + "#have")
             val manager = PerforceConnectionManager.getInstance(project)
             val execResult = runner.executeP4Command(commandArguments.arguments, manager.getConnectionForFile(perforceRoot)!!)
             return if (execResult.exitCode != 0) {
                 LOGGER.error("fstat failed with non-zero exit code")
                 emptyMap()
             } else {
-                var map = emptyMap<Path,Long>()
+                var map = emptyMap<String,FStat>()
                 execResult.allowSafeStdoutUsage { map = parseFstat(it) }
                 map
             }
         }
 
-        private fun parseFstat(result : InputStream) : Map<Path, Long> {
+        private fun parseFstat(result : InputStream) : Map<String, FStat> {
             val reader = LineNumberReader(InputStreamReader(result))
-            val fileMap = HashMap<Path, Long>()
+            val fileMap = HashMap<String, FStat>()
+            FStat()
             try {
                 var line: String?
                 while (reader.readLine().also { line = it } != null) {
-                    if (line!!.startsWith(FSTAT_CLIENT_PREFIX)) {
-                        val file = File(line!!.substring(FSTAT_CLIENT_PREFIX.length))
+                    if (line!!.startsWith(FSTAT_DEPOT_PREFIX)) {
+                        val depotFile = line!!.substring(FSTAT_DEPOT_PREFIX.length)
+                        line = reader.readLine() ?: throw IOException("Unexpected end of file")
+                        if (!line!!.startsWith(FSTAT_CLIENT_PREFIX)) {
+                            LOGGER.warn("Unexpected content in fstat result")
+                            continue
+                        }
+                        val file = line!!.substring(FSTAT_CLIENT_PREFIX.length)
                         line = reader.readLine() ?: throw IOException("Unexpected end of file")
                         if (!line!!.startsWith(FSTAT_HAVEREV_PREFIX)) {
                             LOGGER.warn("Unexpected content in fstat result")
                             continue
                         }
                         val changelist = line!!.substring(FSTAT_HAVEREV_PREFIX.length)
-                        fileMap[file.toPath()] = changelist.toLong()
+                        val fstat = FStat()
+                        fstat.depotFile = depotFile
+                        fstat.clientFile = file
+                        fstat.haveRev = changelist
+                        fileMap[file] = fstat
                     }
                 }
             } catch (e: IOException) {
--- src/main/java/dev/bmac/intellij/indexing/shared/project/SharedIndexPerforceBackendHash.kt
+++ src/main/java/dev/bmac/intellij/indexing/shared/project/SharedIndexPerforceBackendHash.kt
@@ -1,10 +1,11 @@
 package dev.bmac.intellij.indexing.shared.project
 
+import com.google.common.hash.Hashing
 import com.intellij.indexing.shared.platform.hash.SharedIndexContentHash
 
 object SharedIndexPerforceBackendHash : SharedIndexContentHash {
     override val hashLength: Int
-        get() = Long.SIZE_BYTES
+        get() = Long.SIZE_BYTES + (Hashing.sha1().bits() / Byte.SIZE_BITS)
     override val performanceRank: Double
         get() = 400.0
     override val providerId: String
